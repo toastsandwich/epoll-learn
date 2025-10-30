990c06532dcdf2e8d4e810e5ecb3c1eb36b580df
toastsandwich <shreyasxcore2003@gmail.com>
2025-10-30 23:15:31 +0530

    add epoll events plus make srvfd and clntfd non blocking sockets

--- echo_server/main.go
+++ echo_server/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
 	"sync"
 
@@ -20,6 +19,7 @@ func CreateBufferPool(secure bool) *BufferPool {
 	return &BufferPool{
 		sync.Pool{
 			New: func() any {
+				fmt.Println("buffer pool just create a new buffer")
 				return make([]byte, BUFFEERSIZE)
 			},
 		},
@@ -43,46 +43,85 @@ func (b *BufferPool) put(p []byte) {
 var bufferPool = CreateBufferPool(false)
 
 func socket_bind_listen() {
-	fd, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM, 0)
+	srvfd, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM, 0)
 	ifError(err)
-	defer unix.Close(fd)
+	defer unix.Close(srvfd)
+	unix.SetNonblock(srvfd, true) // set server fd to non block so it doesnot block forever while reading
 
-	ifError(unix.SetsockoptInt(fd, unix.SOL_SOCKET, unix.SO_REUSEADDR, 1))
+	ifError(unix.SetsockoptInt(srvfd, unix.SOL_SOCKET, unix.SO_REUSEADDR, 1))
 
 	addr := unix.SockaddrInet4{Port: 9000}
 	copy(addr.Addr[:], []byte{0, 0, 0, 0})
 
-	ifError(unix.Bind(fd, &addr))
+	ifError(unix.Bind(srvfd, &addr))
+	ifError(unix.Listen(srvfd, 10))
+	fmt.Println("echo server is now listening at 0.0.0.0:9000")
+	// this is the epoll instance fd
+	epfd, err := unix.EpollCreate1(unix.EPOLL_CLOEXEC)
+	ifError(err)
 
-	unix.Listen(fd, 10)
+	// now register events to this epfd
+	eventTypes := unix.EPOLLIN | unix.EPOLLERR | unix.EPOLLHUP | unix.EPOLLRDHUP
+	ifError(unix.EpollCtl(epfd, unix.EPOLL_CTL_ADD, srvfd, &unix.EpollEvent{
+		Events: uint32(eventTypes),
+		Fd:     int32(srvfd),
+	}))
 
-	handleConn := func(fd int) {
-		for {
-			buf := bufferPool.get()
-
-			n, err := unix.Read(fd, buf)
-			if err != nil {
-				if err == io.EOF {
-					unix.Write(fd, []byte("bye"))
-					return
-				}
-				fmt.Println(err)
-			}
-			unix.Write(fd, buf[:n])
-			bufferPool.put(buf)
-		}
-	}
-
+	events := make([]unix.EpollEvent, 100) // monitor at max 100 events
 	for {
-		cfd, sockaddr, err := unix.Accept(fd)
+		n, err := unix.EpollWait(epfd, events, -1) // block forever
 		if err != nil {
-			fmt.Println(err)
-			continue
+			if err == unix.EINTR {
+				continue
+			}
+			ifError(err)
 		}
-		fmt.Println("recieved connection from:", sockaddr.(*unix.SockaddrInet4).Addr)
-		go handleConn(cfd)
-	}
+		for i := range n {
+			evt := events[i]
+			efd := int(evt.Fd) // this is event fd
+			if evt.Events&(unix.EPOLLERR|unix.EPOLLHUP|unix.EPOLLRDHUP) != 0 {
+				unix.Close(efd)
+				unix.EpollCtl(epfd, unix.EPOLL_CTL_DEL, efd, nil)
+				continue
+			}
+			// if the events is of server fd, then we have an incoming connection.
+			if efd == srvfd {
+				clntfd, csockaddr, err := unix.Accept(efd)
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
 
+				// now add the client fd to event poll
+				ifError(unix.EpollCtl(epfd, unix.EPOLL_CTL_ADD, clntfd, &unix.EpollEvent{
+					Events: uint32(eventTypes),
+					Fd:     int32(clntfd),
+				}))
+
+				fmt.Println("new connection from", csockaddr.(*unix.SockaddrInet4).Addr)
+			} else { // we are getting data from clients, get a buffer from poll and read the data and echo it
+				buf := bufferPool.get()
+				n, err := unix.Read(efd, buf)
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+				if n == 0 { // this means that client is done with server
+					unix.Close(efd)
+					ifError(unix.EpollCtl(epfd, unix.EPOLL_CTL_DEL, efd, nil))
+					bufferPool.put(buf)
+					continue
+				}
+				_, err = unix.Write(efd, buf[:n])
+				if err != nil {
+					fmt.Println(err)
+					bufferPool.put(buf)
+					continue
+				}
+				bufferPool.put(buf)
+			}
+		}
+	}
 }
 
 func ifError(err error) {
